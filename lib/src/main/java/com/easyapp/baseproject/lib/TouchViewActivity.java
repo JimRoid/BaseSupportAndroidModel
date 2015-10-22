4c0330eeb4dbec7680d4602c705dd918ecdb336a
EasyApp_Jim <jim@easyapp.com.tw>
2015-10-22 15:19:01 +0800

    1.0.9.6

--- lib/src/main/java/com/easyapp/baseproject/lib/TouchViewActivity.java
+++ lib/src/main/java/com/easyapp/baseproject/lib/TouchViewActivity.java
@@ -5,9 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.easyapp.baseproject.lib.View.TouchImageView;
 import com.easyapp.baseproject.lib.asyncTool.DownloadImageTask;
+import com.easyapp.baseproject.lib.tool.Base64Tool;
 import com.easyapp.baseproject.lib.util.Common;
 import com.orhanobut.logger.Logger;
 
@@ -23,26 +27,36 @@ public class TouchViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        FrameLayout frameLayout = new FrameLayout(this);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutParams.gravity = Gravity.CENTER;
+        frameLayout.setLayoutParams(layoutParams);
         touchImageView = new TouchImageView(this);
-        setContentView(touchImageView);
-
+        frameLayout.setBackgroundColor(getResources().getColor(android.R.color.background_dark));
+        frameLayout.addView(touchImageView);
+        setContentView(frameLayout);
         getExtraIntent();
     }
 
     protected void getExtraIntent() {
         Intent intent = getIntent();
         String path = intent.getStringExtra(Common.PATH);
+        Logger.d(path);
 
         if (path.contains("http")) {
             new DownloadImageTask(touchImageView)
                     .execute(path);
-        } else if (path.contains("file")) {
+        } else if (path.contains("/storage")) {
             File file = new File(path);
             if (file.exists()) {
                 Logger.d(path);
                 Bitmap bitmap = BitmapFactory.decodeFile(path);
                 touchImageView.setImageBitmap(bitmap);
             }
+        } else {
+            byte[] decodedString = Base64Tool.decodeBase64(path);
+            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            touchImageView.setImageBitmap(decodedByte);
         }
     }
 
