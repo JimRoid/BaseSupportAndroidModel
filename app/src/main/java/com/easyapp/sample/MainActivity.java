ab1f01723ba1a81d0bb09089c81861dc8ed2b34c
Jim <jim@easyapp.com.tw>
2018-12-18 15:06:26 +0800

    no message

--- app/src/main/java/com/easyapp/sample/MainActivity.java
+++ app/src/main/java/com/easyapp/sample/MainActivity.java
@@ -1,12 +1,8 @@
 package com.easyapp.sample;
 
 
-import com.easyapp.lib.activity.BaseMainActivity;
 import com.easyapp.lib.drawer.BaseDrawerMainActivity;
-import com.easyapp.sample.screen.DiscussCreate;
 import com.easyapp.sample.screen.DiscussList;
-import com.easyapp.sample.screen.DiscussPager;
-import com.easyapp.sample.screen.NoteBook;
 
 /**
  *
@@ -17,8 +13,8 @@ public class MainActivity extends BaseDrawerMainActivity {
     @Override
     protected void initial() {
 
-        ReplaceFragment(NoteBook.instance());
-//        ReplaceFragment(new DiscussList());
+//        ReplaceFragment(NoteBook.instance());
+        ReplaceFragment(new DiscussList());
 //        ReplaceFragment(new DiscussPager());
 //        ReplaceFragment(new DiscussCreate());
     }
