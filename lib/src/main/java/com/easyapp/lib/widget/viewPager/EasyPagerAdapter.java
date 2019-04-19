32d803530e29edebb8373b7ff3ef98c3a4bd4acf
Jim <jim@easyapp.com.tw>
2019-04-19 16:15:01 +0800

    add custom tab

--- lib/src/main/java/com/easyapp/lib/widget/viewPager/EasyPagerAdapter.java
+++ lib/src/main/java/com/easyapp/lib/widget/viewPager/EasyPagerAdapter.java
@@ -1,12 +1,14 @@
 package com.easyapp.lib.widget.viewPager;
 
 
-
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -18,11 +20,43 @@ public class EasyPagerAdapter extends FragmentPagerAdapter {
 
     private ArrayList<Fragment> fragments;
     private ArrayList<String> titles;
+    private ArrayList<Integer> integers;
+
+    //custom
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     public EasyPagerAdapter(FragmentManager fm) {
         super(fm);
         titles = new ArrayList<>();
         fragments = new ArrayList<>();
+    }
+
+    public void initCustomTab(ViewPager viewPager, TabLayout tabLayout) {
+        this.tabLayout = tabLayout;
+        this.viewPager = viewPager;
+
+        viewPager.setAdapter(this);
+        tabLayout.setupWithViewPager(viewPager);
+
+        if (tabLayout.getTabCount() > 0) {
+            for (int i = 0; i < tabLayout.getTabCount(); i++) {
+                TabLayout.Tab tab = tabLayout.getTabAt(i);
+                if (tab != null) {
+                    tab.setCustomView(getTabView(i));
+                }
+            }
+        }
+    }
+
+    /**
+     * custom tabLayout.tab
+     *
+     * @param position
+     * @return
+     */
+    public View getTabView(int position) {
+        return null;
     }
 
     public void addFragment(Fragment fragment) {
@@ -33,6 +67,33 @@ public class EasyPagerAdapter extends FragmentPagerAdapter {
     public void addFragment(Fragment fragment, String title) {
         fragments.add(fragment);
         titles.add(title);
+    }
+
+    public void addFragment(Fragment fragment, int resourceId) {
+        fragments.add(fragment);
+        integers.add(resourceId);
+    }
+
+    public void addFragment(Fragment fragment, String title, int resourceId) {
+        fragments.add(fragment);
+        titles.add(title);
+        integers.add(resourceId);
+    }
+
+    public ArrayList<String> getTitles() {
+        return titles;
+    }
+
+    public void setTitles(ArrayList<String> titles) {
+        this.titles = titles;
+    }
+
+    public ArrayList<Integer> getIntegers() {
+        return integers;
+    }
+
+    public void setIntegers(ArrayList<Integer> integers) {
+        this.integers = integers;
     }
 
     @Nullable
